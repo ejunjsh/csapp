a9673a9f6c7718a0cb48bf0a1f5539844e3f2291
sky <sjj050121014@163.com>
2017-12-03 23:30:55 +0800

    2.71

--- chapter02/src/rotate-left.c
+++ chapter02/src/rotate-left.c
@@ -1,6 +1,15 @@
+/*
+ * rotate-left.c
+ */
 # include <stdio.h>
 # include <assert.h>
 
+
+/*
+ * Do rotate left shift. Assume 0 <= n < w
+ * Example when x = 0x12345678 and w = 32:
+ *   n = 4 -> 0x23456781, n = 20 -> 0x67812345
+ */
 unsigned rotate_left(unsigned x, int n){
     int w=sizeof(unsigned)<<3;
     return x<<n | x >> (w - n -1) >> 1;
